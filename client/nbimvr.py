47f60e8a4a0d129095842f119e245c83b9fa8c67
Bård Lahn <bard@lahn.no>
2026-04-22 12:35:21 +0200

    Bugfix

--- client/nbimvr.py
+++ client/nbimvr.py
@@ -1,10 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List, Optional
-
-from cachetools import LRUCache
+from collections import OrderedDict
 
 from model import Company, MeetingWithVotes
 
+class LRUDict(OrderedDict):
+    def __init__(self, maxsize=128):
+        super().__init__()
+        self.maxsize = maxsize
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.move_to_end(key)
+        super().__setitem__(key, value)
+        if len(self) > self.maxsize:
+            oldest = next(iter(self))
+            del self[oldest]
+
+    def __getitem__(self, key):
+        value = super().__getitem__(key)
+        self.move_to_end(key)
+        return value
 
 class NBIMVR(ABC):
     base_url: str = "https://vd.a.nbim.no/v1"
@@ -13,7 +29,7 @@ class NBIMVR(ABC):
         super().__init__()
 
         self.headers: Dict[str, str] = {"x-api-key": api_key}
-        self.request_timer_dict = LRUCache(maxsize=128)
+        self.request_timer_dict = LRUDict(128)
 
     @abstractmethod
     def get_tickers(self) -> List[str]:
